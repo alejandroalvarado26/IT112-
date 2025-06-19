71ea0362acd10c54d53436e1c5722b42960cf08a
alejandroalvarado26 <alejandroalvaradomora26@gmail.com>
2025-06-18 17:27:36 -0700

    HW11

--- myDjangoProject/myDjangoApp/admin.py
+++ myDjangoProject/myDjangoApp/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Bands
 # Register your models here.
 
-admin.site.register(Bands)
+@admin.register(Bands)
+class BandsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'genre')
+    list_filter = ('genre',)
+    search_fields = ('name',)
